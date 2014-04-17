4496e3aad65e79967f6f7db73c29f72e15be238a
Sourya Sharma <sourya.s7@gmail.com>
2014-04-17 14:15:47 -0600

    Updating the structure

--- src/lexer/number.h
+++
@@ -1,9 +0,0 @@
-#ifndef T_NUMBER_H
-#define T_NUMBER_H
-#include "token.h"
-class Number : public Token {
-public:
-    int number;
-    Number(int pnumber, Tags ptag) : Token(ptag), number(pnumber) {}
-};
-#endif
--- src/lexer/real.h
+++
@@ -1,9 +0,0 @@
-#ifndef T_REAL_H
-#define T_REAL_H
-#include "token.h"
-class Real : public Token {
-    double number;
-public:
-    Real(double pnumber, Tags ptag) : Token(ptag), number(pnumber) {}
-};
-#endif

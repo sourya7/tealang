57ebb28fed838b3c2bcfd24cadad2de1d2ea6d44
Sourya Sharma <sourya.s7@gmail.com>
2014-04-23 11:59:28 -0600

    Adding a newline

--- src/tlang.cxx
+++ src/tlang.cxx
@@ -35,6 +35,7 @@ int main(int argc, char* argv[]){
         parser = new TParser(&cin);                          
     }                                                  
     parser->Parse();
+    cout << endl;
 }                                                      
 
 

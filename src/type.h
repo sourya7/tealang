5a029c5850246bab3dc0f733e09908e80a628bf3
Sourya Sharma <sourya.s7@gmail.com>
2014-04-25 11:35:37 -0600

    Adding type helpers

--- src/type.h
+++ src/type.h
@@ -9,10 +9,11 @@ using std::string;
  */
 
 class TObj;
-enum TType {
+
+enum class TType {
     INTEGER = 1,
-    REAL,
-    BOOL,
+    DOUBLE,
+    BOOLEAN,
     STRING,
     OBJECT,
     NIL
@@ -24,6 +25,41 @@ union TValue {
     bool b;
     string* str;
     TObj* object;
+};
+
+class TObj {
+private:
+    TType type = TType::NIL;
+    TValue* value = nullptr;
+    TObj() {} 
+public:
+    static const TObj NIL;
+    bool IsBool() { return false; }
+    bool IsInteger() { return false; }
+    bool IsString() { return false; }
+    bool IsDouble() { return false; } 
+    bool IsNil() { return false; }
+    bool IsTrue() { return false; }
+
+    TObj(long i){
+        type = TType::INTEGER;
+    }
+
+    TObj(double d){
+        type = TType::DOUBLE;
+    }
+
+    TObj(bool b){
+        type = TType::BOOLEAN;
+    }
+
+    TObj(string* s){
+        type = TType::STRING;
+    }
+
+    TObj(TObj* o){
+        type = TType::OBJECT;
+    }
 };
 
 #endif

0a3117db688042b1303817d78bc501847ae8a1a5
Sourya Sharma <sourya.s7@gmail.com>
2014-04-25 09:10:50 -0600

    Adding a nodetype and removing the need to always inherit to obtain a property

--- src/node.h
+++ src/node.h
@@ -1,13 +1,17 @@
 #ifndef T_NODE_H
 #define T_NODE_H
 
+enum class NodeType {NODE, SEQ, CALL, PARAM, IFSTMT, FSTMT, ASSIGN, EXPR};
 class Node {
 protected:
     Node* left;
     Node* right;
+    NodeType type = NodeType::NODE;
 public:
     Node() {} 
+    Node(NodeType t) : type(t) {} 
     Node(Node* l, Node* r) : left(l), right(r) {} 
+    Node(NodeType t, Node *l, Node* r) : type(t), left(l), right(r) {} 
     void SetLeft(Node* l) { left = l; }
     void SetRight(Node* r) { right = r; }
 };

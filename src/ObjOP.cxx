4ccf84ee65d44e9b18b24bcd032d9930495ad5e4
Sourya Sharma <sourya.s7@gmail.com>
2014-05-05 16:09:10 -0600

    Renaming Num to ObjOP

---
+++ src/ObjOP.cxx
@@ -0,0 +1,19 @@
+#include "ObjOP.h"
+namespace ObjOP {
+Object* Add(Object* a, Object* b){ 
+    Object* ob = *a + b;
+    return ob; 
+}
+Object* Sub(Object* a, Object* b){
+    Object* ob = *a - b;
+    return ob;
+}
+Object* Mul(Object* a, Object* b){
+    Object* ob = *a * b;
+    return ob;
+}
+Object* Equal(Object* a, Object* b){
+    Object* ob = *a == b;
+    return ob;
+}
+}
